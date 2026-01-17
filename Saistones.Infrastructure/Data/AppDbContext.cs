9db7bbd5af55747d6505f4ea17e1b09af8cdd85b
ganeshpalanipro <ganeshpalanipro@gmail.com>
2026-01-17 21:46:18 +0530

    feat(infrastructure): add EF Core DbContext and UserRepository

---
+++ Saistones.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saistones.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saistones.Infrastructure.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ Saistones.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saistones.Domain.Entities;
+using Saistones.Domain.Interfaces;
+using Saistones.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saistones.Infrastructure.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _db;
+
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public Task<User?> GetByEmailAsync(string email)
+        {
+            return _db.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}
