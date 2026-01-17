a3ab4a166ca9c69354769b40529cdbaa98bf1c88
ganeshpalanipro <ganeshpalanipro@gmail.com>
2026-01-17 21:32:58 +0530

    feat(application): add UserService and UserDto

---
+++ Saistones.Application/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Saistones.Application.DTOs;
+using Saistones.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saistones.Application.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDto?> GetByEmailAsync(string email)
+        {
+            var user = await _userRepository.GetByEmailAsync(email);
+            if (user == null) return null;
+
+            return new UserDto
+            {
+                Email = user.Email,
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
